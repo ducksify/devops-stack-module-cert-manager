e57336aaba72dfcea47f7582debddb8990f85851
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-01-10 11:28:40 -0500

    remove eks DNS-01 support for now
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,9 +1,9 @@
-module "eks" {
-  count = contains(var.profiles, "eks") ? 1 : 0
-
-  source = "./eks"
-
-  cluster_name            = var.cluster_name
-  base_domain             = var.base_domain
-  cluster_oidc_issuer_url = var.cert_manager.cluster_oidc_issuer_url
-}
+#module "eks" {
+#  count = contains(var.profiles, "eks") ? 1 : 0
+#
+#  source = "./eks"
+#
+#  cluster_name            = var.cluster_name
+#  base_domain             = var.base_domain
+#  cluster_oidc_issuer_url = var.cert_manager.cluster_oidc_issuer_url
+#}
