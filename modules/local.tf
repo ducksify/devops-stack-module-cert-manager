979cf9667f280757dd31b4afba66938c3d9a261f
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-01-17 08:58:49 -0500

    fix: remove legacy cert-manager variable in template
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- modules/local.tf
+++ modules/local.tf
@@ -2,7 +2,6 @@ locals {
   profiles_yaml = [ for i in var.profiles : templatefile("${path.module}/profiles/${i}.yaml", {
       cluster_name = var.cluster_name,
       base_domain  = var.base_domain,
-      cert_manager = var.cert_manager,
   }) ]
   all_yaml = concat(local.profiles_yaml, var.extra_yaml)
 }
